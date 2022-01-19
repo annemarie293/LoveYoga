6e9eba17054d03360982c5a61c3a949e97381941
annemarie293 <annemarie293@gmail.com>
2022-01-19 22:34:47 +0000

    created shop app, model and images

---
+++ shop/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+# Create your models here.
+
+class ShopProducts(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'ShopProducts'
+
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
