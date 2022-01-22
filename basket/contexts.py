a526f982bab8fe7739dc966d31dcbc8d92cb82d2
annemarie293 <annemarie293@gmail.com>
2022-01-22 21:28:57 +0000

    formattin view_basket page

--- basket/contexts.py
+++ basket/contexts.py
@@ -7,13 +7,11 @@ def basket_contents(request):
 
     basket_items = []
     classes_total = 0
-    product_total = 0
+    products_total = 0
     sub_total = 0
-    product_count = 0
+    products_count = 0
     classes_count = 0
     basket = request.session.get('basket', {})
-    print('context basket')
-    print(basket)
 
     for item_id, item_data in basket.items():
         category = item_data['category']
@@ -37,30 +35,26 @@ def basket_contents(request):
             quantity = item_data['quantity']
             category = item_data['category']
 
-            product = get_object_or_404(ShopProducts, id=item_id)
-            product_total += quantity * product.price
-            product_count += quantity
+            products = get_object_or_404(ShopProducts, id=item_id)
+            products_total += quantity * products.price
+            products_count += quantity
             basket_items.append({
                 'item_id': item_id, 
                 'quantity': quantity,
                 'category': category,
-                'product': product
+                'products': products
             })
         
     delivery = 5
-    sub_total = classes_total + product_total
+    sub_total = classes_total + products_total
     grand_total = sub_total + delivery
-
-    print(sub_total)
-    print(grand_total)
-    print(basket_items)
 
     context = {
         'basket_items': basket_items,
         'sub_total': sub_total,
-        'product_total': product_total,
+        'products_total': products_total,
         'classes_total': classes_total,
-        'product_count': product_count,
+        'products_count': products_count,
         'classes_count': classes_count,
         'delivery': delivery,
         'grand_total': grand_total,
