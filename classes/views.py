41a258ebc12ed35c4842840be948a2460b31b819
annemarie293 <annemarie293@gmail.com>
2022-01-23 15:56:00 +0000

    create order form template

--- classes/views.py
+++ classes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import YogaClass
 
 # Create your views here.
