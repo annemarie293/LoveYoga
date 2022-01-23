b9b816d57802e3d989cf9e4120bdd3b5141c1167
annemarie293 <annemarie293@gmail.com>
2022-01-23 16:25:04 +0000

    basket template hide product/class section if not in basket

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import OrderForm
 
 # Create your views here.
+
+def checkout(request):
+    """
+    A view to display the chekcout page
+    """
+    basket = request.session.get('basket', {})
+    if not basket:
+        # messages.error(request, "You havent added anything to your basket yet!")
+        return redirect(reverse('classes'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+
+    context = {
+        'order_form': order_form
+    }
+
+    return render (request, template, context)
