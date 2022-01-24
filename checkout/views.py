311e84bddf852a895df778494675f71789983885
annemarie293 <annemarie293@gmail.com>
2022-01-24 21:04:05 +0000

    updated checkout view to cache data for webhook

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,10 @@
 import stripe
+import json
 
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 from django.contrib import messages
 from django.conf import settings
+from django.views.decorators.http import require_POST
 from .forms import OrderForm
 
 from shop.models import ShopProducts
@@ -11,6 +13,22 @@ from .models import Order, OrderLineItem
 from basket.contexts import basket_contents
 
 # Create your views here.
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('secret')[0]    
+        stripe_secret_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata = {
+            'username': request.user,
+            'save_info': request.POST.get('save_info'),
+            'basket': json.dumps(request.session.get('basket', {}))
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, "Sorry we can't process your payment right now, please contact us for support")
+        return HttpResponse(content=e, status=400)
+
 
 def checkout(request):
     """
