e1d4c97709ef745b7754f36f70eb41c74532f482
annemarie293 <annemarie293@gmail.com>
2022-01-25 23:47:25 +0000

    view and URL for previous user orders

--- profiles/views.py
+++ profiles/views.py
@@ -20,8 +20,6 @@ def profile(request):
             messages.success(request, 'Your new profile information has been saved')
 
     orders = profile.orders.all()
-    print ("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM")
-    print(orders)
 
     form = UserProfileForm(instance=profile)
     template = 'profiles/profile.html'
@@ -31,6 +29,24 @@ def profile(request):
         'form': form, 
         'orders': orders,
         'on_profile': True
+    }
+
+    return render(request, template, context)
+
+
+def user_orders(request, order_number):
+    """
+    A view to display the previous order made by the user
+    """
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, f'This is a record of your previous order # {order_number}')
+
+    template = "checkout/checkout_success.html"
+
+    context = {
+        'order': order, 
+        'from_profile_page': True,
     }
 
     return render(request, template, context)
