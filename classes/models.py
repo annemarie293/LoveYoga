8db1e3cc396a47f835e9abcf71508a34e05ea3a4
annemarie293 <annemarie293@gmail.com>
2022-01-27 00:47:01 +0000

    add links to add product/class/trainer

--- classes/models.py
+++ classes/models.py
@@ -23,19 +23,19 @@ class YogaClass(models.Model):
         ('high', 'High')
     ]
 
-    name = models.CharField(max_length=250, null=False, blank=False)
-    category = models.CharField(max_length=7, null=False, blank=False)   
-    sku = models.CharField(max_length=254, null=False, blank=False)
-    description = models.TextField(null=False, blank=False)
-    practice = models.CharField(max_length=250, null=False, blank=False)
+    name = models.CharField(max_length=250, null=True, blank=False)
+    category = models.CharField(max_length=7, null=True, blank=False)   
+    sku = models.CharField(max_length=254, null=True, blank=False)
+    description = models.TextField(null=True, blank=False)
+    practice = models.CharField(max_length=250, null=True, blank=False)
     level = models.CharField(max_length=250, null=True, blank=False, choices=level_choices)
     intensity = models.CharField(max_length=250, null=True, blank=False, choices=intensity_choices)
-    session_duration = models.IntegerField(null=False, blank=False)
-    series_duration = models.IntegerField(null=False, blank=False)
-    equipment = models.CharField(max_length=250, null=False, blank=False)
+    session_duration = models.IntegerField(null=True, blank=False)
+    series_duration = models.IntegerField(null=True, blank=False)
+    equipment = models.CharField(max_length=250, null=True, blank=False)
     trainer = models.ForeignKey(Trainer, null=True, blank=False, on_delete=models.SET_NULL)
-    price = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False)
-    image = models.ImageField(null=False, blank=False)
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=False)
+    image = models.ImageField(null=True, blank=False)
 
     def __str__(self):
         return self.name
--- shop/models.py
+++ shop/models.py
@@ -8,12 +8,12 @@ class ShopProducts(models.Model):
     class Meta:
         verbose_name_plural = 'ShopProducts'
 
-    sku = models.CharField(max_length=254, null=False, blank=False)
-    category = models.CharField(max_length=7, null=False, blank=False)
-    name = models.CharField(max_length=250, null=False, blank=False)
-    description = models.TextField(null=False, blank=False)
-    price = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False)
-    image = models.ImageField(null=False, blank=False)
+    sku = models.CharField(max_length=254, null=True, blank=False)
+    category = models.CharField(max_length=7, null=True, blank=False)
+    name = models.CharField(max_length=250, null=True, blank=False)
+    description = models.TextField(null=True, blank=False)
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=False)
+    image = models.ImageField(null=True, blank=False)
 
     def __str__(self):
         return self.name
