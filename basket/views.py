7601438ac9f8aa449a6cd9818f8dbf240d25a820
annemarie293 <annemarie293@gmail.com>
2022-01-27 15:06:10 +0000

    view to adjust qty, fix bug to add items with same id

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, reverse, redirect, get_object_or_404
+from django.shortcuts import render, reverse, redirect, get_object_or_404, HttpResponse
 from django.contrib import messages
 from shop.models import ShopProducts
 from classes.models import YogaClass
@@ -9,6 +9,8 @@ from classes.models import YogaClass
 def view_basket(request):
     """ A view to return the page displaying all the shop products"""
 
+    basket = request.session.get('basket', {})
+    print(basket)
     context = {
     }
     return render(request, 'basket/view_basket.html', context)
@@ -28,6 +30,9 @@ def add_product_to_basket(request, item_id):
         if category in basket[item_id]['category'] == "product":
             basket[item_id]['quantity'] += quantity
             messages.success(request, f'You now have {basket[item_id]["quantity"]} x {product.name} in your basket')
+        else:
+            basket[item_id] = {'quantity': quantity, 'category': category}
+            messages.success(request, f'{product.name} is now added to your basket')
     else:
         basket[item_id] = {'quantity': quantity, 'category': category}
         messages.success(request, f'{product.name} is now added to your basket')
@@ -47,9 +52,30 @@ def add_class_to_basket(request, item_id):
     if item_id in list(basket.keys()):
         if category in basket[item_id]['category'] == "class":
             messages.info(request, f'It looks like you have already added {classes.name} to your basket!')
+        else:
+            basket[item_id] = {'quantity': quantity, 'category': category}
+            messages.success(request, f'{classes.name} is now added to your basket')
     else:
         basket[item_id] = {'quantity': quantity, 'category': category}
         messages.success(request, f'{classes.name} is now added to your basket')
 
     request.session['basket'] = basket
     return redirect(redirect_url)
+
+def update_basket(request, item_id):
+    """ update qty from basket page"""
+
+    quantity = int(request.POST.get('quantity'))
+    basket = request.session.get('basket', {})
+    
+    if basket[item_id]['category'] == "product":
+        if quantity > 0:
+            basket[item_id] = quantity
+            messages.success(request, f'Item quantity has been updated to {quantity}')
+        else:
+            basket.pop[item_id]
+            messages.success(request, f'{Item.name} has been removed from your basket')
+
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket'))
+
