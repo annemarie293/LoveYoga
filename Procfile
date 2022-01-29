0d65223d9c9a135976b3395f4126ef3e59308aa7
annemarie293 <annemarie293@gmail.com>
2022-01-29 01:53:09 +0000

    set databases and hosts

---
+++ Procfile
@@ -0,0 +1,1 @@
+web:gunicorn love_yoga.wsgi:application
--- love_yoga/settings.py
+++ love_yoga/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-fyxx2j@id4t-id$*yq#xu++_^vl89rzz-jt&8nggxpqck-(qub
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['loveyoga.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -102,12 +102,18 @@ WSGI_APPLICATION = 'love_yoga.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+
+if DATABASE_URL in os.environ:
+    DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 
