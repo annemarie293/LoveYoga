eaf5453d7c1176086dd001f49edb625b9fb354e6
annemarie293 <annemarie293@gmail.com>
2022-01-29 02:07:49 +0000

    set secret ket and debug

--- love_yoga/settings.py
+++ love_yoga/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fyxx2j@id4t-id$*yq#xu++_^vl89rzz-jt&8nggxpqck-(qub'
+SECRET_KEY =os.envoron.get('SECRET_KEY', "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "DEVELOPMENT" in os.environ
 
 ALLOWED_HOSTS = ['loveyoga.herokuapp.com', 'localhost']
 
@@ -178,6 +178,7 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 SITE_ID = 1
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
