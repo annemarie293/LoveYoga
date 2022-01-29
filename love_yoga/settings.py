2526307c2730ad2f4ff537c33dcd52d964f3a617
annemarie293 <annemarie293@gmail.com>
2022-01-29 21:50:26 +0000

    updated debug

--- love_yoga/settings.py
+++ love_yoga/settings.py
@@ -25,7 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "DEVELOPMENT" in os.environ
+# DEBUG = "DEVELOPMENT" in os.environ
+
+DEBUG = True
+
 
 ALLOWED_HOSTS = ['loveyoga.herokuapp.com', 'localhost']
 
