4aebf61e4b753bc233c49536b22d2cf7c6518dbb
annemarie293 <annemarie293@gmail.com>
2022-01-30 19:03:03 +0000

    html validator fixes

--- profiles/forms.py
+++ profiles/forms.py
@@ -28,9 +28,6 @@ class UserProfileForm(forms.ModelForm):
         for field in self.fields:
             if field != 'default_country':
                 if self.fields[field].required:
-                    placeholder = f'{placeholders[field]}'
-                else:
-                    placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+                    self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'profile-input'
             self.fields[field].label = False
