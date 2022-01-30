3f0d44465dfe6a55e072f1f980f033dae7169b66
annemarie293 <annemarie293@gmail.com>
2022-01-30 14:13:38 +0000

    add about page, uncomment form.submit()

--- home/views.py
+++ home/views.py
@@ -6,3 +6,8 @@ from django.shortcuts import render
 def index(request):
     """ A view to return the home page """
     return render(request, 'home/index.html')
+
+
+def about_us(request):
+    """ A view to return the About Us page """
+    return render(request, 'home/about.html')
